12f8d60bb389bd8277eb978b0cd001b2046f445b
chris_boss <christianwondeson22@gmail.com>
2021-03-11 06:46:17 -0800

    Add project files.

---
+++ Assets/script/chage_Scene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class chage_Scene : MonoBehaviour
+{
+
+    public void change_sene(string scenename) {
+        Application.LoadLevel(scenename);
+    }
+}
